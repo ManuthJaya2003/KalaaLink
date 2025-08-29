6e8b3577f652d7312f1595f15f5b852f82594a2d
Manuth Jayasekara <mej20030829@gmail.com>
2025-08-29 09:33:56 +0530

    Fully functional Artist Booking Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 // import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import Home from './Components/Home/Home.js';
 import ArtistManagerDashboard from './Components/Manuth/ArtistManagerDashboard/ArtistManagerDashboard.js';
-import React from 'react';
-import { Route,Routes } from 'react-router-dom';
 import ManageArtists from './Components/Manuth/ManageArtists/ManageArtists.js';
 import AddArtist from './Components/Manuth/AddArtist/AddArtist.js';
 import Artists from './Components/Artists/Artists.js';
 import UpdateArtist from './Components/Manuth/UpdateArtist/UpdateArtist.js';
 import Applications from './Components/Manuth/Applications/Applications.js';
-import SignUp from './Components/SignUp/SignUp.js'
-import Overview from './Components/Manuth/Overview/Overview.js'
+import SignUp from './Components/SignUp/SignUp.js';
+import Overview from './Components/Manuth/Overview/Overview.js';
 import ArtistRegistration from './Components/Manuth/ArtistRegistration/ArtistRegistration.js';
 import Login from './Components/Login/Login.js';
 import ProfessionalLogin from './Components/Manuth/ProfessionalLogin/ProfessionalLogin.js';
@@ -19,35 +20,53 @@ import ArtistLogin from './Components/Manuth/ArtistLogin/ArtistLogin.js';
 import ArtistPortfolio from './Components/Manuth/ArtistPortfolio/ArtistPortfolio.js';
 import ArtistDashboard from './Components/Manuth/ArtistDashboard/ArtistDashboard.js';
 import ArtistEditProfile from './Components/Manuth/ArtistEditProfile/ArtistEditProfile.js';
+import BookArtist from './Components/Manuth/BookArtist/BookArtist.js';
 
 function App() {
   return (
     <div>
       <React.Fragment>
         <Routes>
-          <Route path="/" element = {<Home/>}/>
-          <Route path="/mainhome" element = {<Home/>}/>
-          <Route path="/artists" element = {<Artists/>}/>
-          <Route path="/artistManager" element = {<ArtistManagerDashboard/>}/>
-          <Route path="/manage_artists" element = {<ManageArtists/>}/>
+          {/* Home */}
+          <Route path="/" element={<Home />} />
+          <Route path="/mainhome" element={<Home />} />
+
+          {/* Artists */}
+          <Route path="/artists" element={<Artists />} />
+          
+          {/* Artist Manager */}
+          <Route path="/artistManager" element={<ArtistManagerDashboard />} />
+          <Route path="/manage_artists" element={<ManageArtists />} />
           <Route path="/manage_artists/:artist_id" element={<UpdateArtist />} />
-          <Route path="/addArtist" element = {<AddArtist/>}/>
-          <Route path="/applications" element = {<Applications/>}/>
-          <Route path="/signup" element = {<SignUp/>}/>
-          <Route path="/overview" element = {<Overview/>}/>
-          <Route path="/register" element = {<ArtistRegistration/>}/>
-          <Route path="/login" element = {<Login/>}/>
-          <Route path="/professional_login" element = {<ProfessionalLogin/>}/>
-          <Route path="/admindashboard" element = {<AdminDashboard/>}/>
-          <Route path="/eventmanagerdashboard" element={<EventManagerDashboard/>}/>
-          <Route path="/artist_login" element={<ArtistLogin/>}/>
-          <Route path="/portfolio" element={<ArtistPortfolio/>} />
-          <Route path="/artistdashboard" element={<ArtistDashboard/>}/>
-          <Route path="/artist/editprofile" element={<ArtistEditProfile/>}/>
+          <Route path="/addArtist" element={<AddArtist />} />
+          <Route path="/applications" element={<Applications />} />
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/register" element={<ArtistRegistration />} />
+          <Route path="/artist_login" element={<ArtistLogin />} />
+
+          {/* SignUp/Login */}
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/professional_login" element={<ProfessionalLogin />} />
+
+          {/* Admin */}
+          <Route path="/admindashboard" element={<AdminDashboard />} />
+
+          {/* Event Manager */}
+          <Route path="/eventmanagerdashboard" element={<EventManagerDashboard />} />
+
+          {/* Artist */}
+          <Route path="/portfolio" element={<ArtistPortfolio />} />
+          <Route path="/artistdashboard" element={<ArtistDashboard />} />
+          <Route path="/artist/editprofile" element={<ArtistEditProfile />} />
+
+          {/* ✅ Book Artist Page */}
+          {/* This route expects `artistId` to be passed via `state` from navigate */}
+          <Route path="/bookArtist" element={<BookArtist />} />
         </Routes>
       </React.Fragment>
     </div>
-  );  
+  );
 }
 
 export default App;
